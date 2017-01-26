622e2be584ca2b52f9d8cd3752002ddd297d8653
unknown <2038567g@student.gla.ac.uk>
2017-01-26 18:48:56 +0000

    ÂEnd of Chapter 5

--- rango/urls.py
+++ rango/urls.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
-
-# Create your url here.
-
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from rango import views
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns = [
+url(r'^$', views.index, name='index'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-urlpatterns = patterns('',
-        url(r'^$', views.index, name='index'))
+
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -14,6 +14,18 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print(__file__)
+print(os.path.dirname(__file__))
+print(os.path.dirname(os.path.dirname(__file__)))
+
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [STATIC_DIR]
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+
+
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
 
 
 # Quick-start development settings - unsuitable for production
@@ -55,7 +67,7 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -63,6 +75,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+				'django.template.context_processors.media',
             ],
         },
     },
@@ -118,4 +131,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+
 STATIC_URL = '/static/'
+
+
+
+
